d57d9a53812f3968b9ba18faad4c0a42a2c9b8bf
Riya Tyagi <riya.tyagi@dixoninfo.com>
2024-02-28 18:02:37 +0530

    Updating NewsMonkey App

---
+++ Riya_Learning/Practice/web_Dev/cwh_react_learning/22-40_NewsMonkey/newsmonkey/src/components/NewsItems.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export class NewsItems extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div>
+        <div className="card" >
+          <img src={!imageUrl? "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2024/02/26/2627149-samsung-galaxy-ring.jpg":imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a rel="noreferrer" href={newsUrl} target="blank" className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItems;
