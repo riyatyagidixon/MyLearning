2158dce5110fabb555ea4c1e00263c00e851d085
Riya Tyagi <riya.tyagi@dixoninfo.com>
2024-03-05 17:35:35 +0530

    Complete News app

--- Riya_Learning/Practice/web_Dev/cwh_react_learning/22-40_NewsMonkey/newsmonkey/src/components/NewsItems.js
+++ Riya_Learning/Practice/web_Dev/cwh_react_learning/22-40_NewsMonkey/newsmonkey/src/components/NewsItems.js
@@ -1,35 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItems extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-    return (
-      <div>
-        <div className="card" >
-          <div style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            position: 'absolute',
-            right: '0'
-          }
-          }>
-            <span className="badge rounded-pill bg-danger">
-              {source}
-            </span>
-          </div>
-          <img src={!imageUrl ? "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2024/02/26/2627149-samsung-galaxy-ring.jpg" : imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toUTCString()}</small></p>
-            <a rel="noreferrer" href={newsUrl} target="blank" className="btn btn-sm btn-dark">
-              Read More
-            </a>
-          </div>
+const NewsItems = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div>
+      <div className="card" >
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'absolute',
+          right: '0'
+        }
+        }>
+          <span className="badge rounded-pill bg-danger">
+            {source}
+          </span>
         </div>
-      </div >
-    );
-  }
+        <img src={!imageUrl ? "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2024/02/26/2627149-samsung-galaxy-ring.jpg" : imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toUTCString()}</small></p>
+          <a rel="noreferrer" href={newsUrl} target="blank" className="btn btn-sm btn-dark">
+            Read More
+          </a>
+        </div>
+      </div>
+    </div >
+  );
+
 }
 
 export default NewsItems;
--- Riya_Learning/Practice/web_Dev/cwh_react_learning/22-40_NewsMonkey/newsmonkey/src/components/Spinner.js
+++ Riya_Learning/Practice/web_Dev/cwh_react_learning/22-40_NewsMonkey/newsmonkey/src/components/Spinner.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React from "react"
 import loading from "../images/loading.gif"
 
-export class Spinner extends Component {
-  render() {
-    return (
-      <div>
-        <div className="text-center">
-          <img className='my-3' src={loading} alt='loading' />
-        </div>
+const Spinner = () => {
+  return (
+    <div>
+      <div className="text-center">
+        <img className='my-3' src={loading} alt='loading' />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Spinner
