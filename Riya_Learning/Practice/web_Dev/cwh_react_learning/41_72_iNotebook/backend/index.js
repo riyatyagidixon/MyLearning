2053f5482f67cff5379e4a5fd3e6fdd93cefbf3e
Riya Tyagi <riya.tyagi@dixoninfo.com>
2024-03-27 14:31:07 +0530

    iNotebook project with reactjs

--- Riya_Learning/Practice/web_Dev/cwh_react_learning/41_72_iNotebook/backend/index.js
+++ Riya_Learning/Practice/web_Dev/cwh_react_learning/41_72_iNotebook/backend/index.js
@@ -5,11 +5,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use(express.json())
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) =>
+    res.send('Hello Riya!')
+)
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on https://localhost: ${port}`)
 })
 
